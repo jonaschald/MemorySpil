81664947e4a9f2145b62ae7a923f61ff7e1af234
Jonas <jonhal01@easv365.dk>
2025-10-03 10:11:10 +0200

    Add images

--- src/main/java/com/example/memoryspil/MemorySpil.java
+++ src/main/java/com/example/memoryspil/MemorySpil.java
@@ -3,6 +3,7 @@ package com.example.memoryspil;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +11,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(new Pane(), 1440, 1024);
+        stage.setTitle("Memory Spil");
         stage.setScene(scene);
         stage.show();
     }
