a0f31af6fc713f9acf2c3fcb579dc353f5ecab3f
Jonas <jonhal01@easv365.dk>
2025-10-08 09:52:22 +0200

    Add kort valg

--- src/main/java/com/example/memoryspil/Brik.java
+++ src/main/java/com/example/memoryspil/Brik.java
@@ -7,19 +7,29 @@ import javafx.scene.image.ImageView;
 import javafx.util.Duration;
 
 public class Brik extends ImageView {
-    private Image forside, bagside;
-    private Brik par;
+    private Image forside, bagside; // Billeder af forsiden og bagsiden af kortet
+    public Brik par; // Det kort, dette kort er i par med
+    private boolean klikkede = false; // Har spilleren klikket på kortet?
+    private MemorySpil spil;
 
-    public Brik(int x, int y, String filnavn) {
+    /**
+     * Brik - Skab en ny brik, der befinder sig ved x og y med navnet af et billede
+     */
+    public Brik(int x, int y, String filnavn, MemorySpil server) {
         forside = new Image(getClass().getResource(filnavn).toString());
         bagside = new Image(getClass().getResource("bagside.png").toString());
         setImage(bagside);
 
         setX(x);
         setY(y);
+
+        spil = server;
     }
 
-    public Brik(int x, int y, String filnavn, Brik par) {
+    /**
+     * Brik - Skab en ny brik, der befinder sig ved x og y med navnet af et billede og den brik, det er parret med
+     */
+    public Brik(int x, int y, String filnavn, Brik par, MemorySpil server) {
         forside = new Image(getClass().getResource(filnavn).toString());
         bagside = new Image(getClass().getResource("bagside.png").toString());
         setImage(bagside);
@@ -28,31 +38,61 @@ public class Brik extends ImageView {
         setY(y);
 
         this.par = par;
+        spil = server;
     }
 
     public void klik() {
-        visForside();
+        if (klikkede) return;
+        klikkede = true;
+
+        if (getImage() == bagside) {
+            visForside().setOnFinished(e -> {
+                spil.vælgKort(this);
+            });
+        } else {
+            visBagside().setOnFinished(e -> {
+                spil.vælgKort(this);
+            });;
+        }
     }
 
-    private void klikAnimation(Image billede) {
-        ScaleTransition skalerNed = new ScaleTransition(Duration.millis(500), this);
+    private SequentialTransition klikAnimation(Image billede) {
+        ScaleTransition skalerNed = new ScaleTransition(Duration.millis(250), this);
         skalerNed.setToX(0);
         skalerNed.setOnFinished(e -> {
             setImage(billede);
         });
 
-        ScaleTransition skalerOp = new ScaleTransition(Duration.millis(500), this);
+        ScaleTransition skalerOp = new ScaleTransition(Duration.millis(250), this);
         skalerOp.setToX(1);
+        skalerOp.setOnFinished(e -> {
+            klikkede = false;
+        });
 
         SequentialTransition animation = new SequentialTransition(skalerNed, skalerOp);
         animation.play();
+
+        return animation;
     }
 
-    public void visForside() {
-        klikAnimation(forside);
+    public SequentialTransition visForside() {
+        return klikAnimation(forside);
     }
 
-    public void visBagside() {
-        klikAnimation(bagside);
+    public SequentialTransition visBagside() {
+        return klikAnimation(bagside);
+    }
+
+    public void skabPar() {
+
+    }
+
+    public boolean erParret(Brik b) {
+        return par == b;
+    }
+
+    public void fjern() {
+        klikkede = true; // Vi har fundet et par, så denne brik kan ikke klikkes mere
+
     }
 }
--- src/main/java/com/example/memoryspil/MemorySpil.java
+++ src/main/java/com/example/memoryspil/MemorySpil.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -36,8 +38,9 @@ public class MemorySpil extends Application {
             "vaskebjørn.jpg",
             "vildsvin.jpg"
     }; // 25 forskellige kort
+    private Brik valgtKort;
+    private int antalTræk = 0;
     private boolean klik = false;
-    private Brik kort1, kort2;
 
     @Override
     public void start(Stage stage) {
@@ -48,24 +51,60 @@ public class MemorySpil extends Application {
         stage.setScene(scene);
         stage.show();
 
-        int x = 0;
-        while (x < 4) {
-            root.getChildren().add(nytKort(x * 125, 0, "abe.jpg"));
+        Text træk = new Text(44, 88, "Antal træk: " + antalTræk);
+        træk.setFont(new Font(36));
+        root.getChildren().add(træk);
+
+        int x = 1;
+        int y = 1;
+        while (x < 10 + 1) {
+            while (y < 5 + 1) {
+                root.getChildren().add(nytKort(x * 125, y * 125, "abe.jpg"));
+                ++y;
+            }
 
             ++x;
+            y = 1;
         }
     }
 
     public Brik nytKort(int x, int y, String filnavn) {
-        Brik b = new Brik(x, y, filnavn);
+        Brik b = new Brik(x, y, filnavn, this);
         b.setOnMouseClicked(e -> {
-            if (klik) {return;}
+            if (klik) return;
             klik = true;
 
             b.klik();
-
         });
 
         return b;
+    }
+
+    public void vælgKort(Brik b) {
+        ++antalTræk;
+
+        // Er dette kort allerede blevet valgt?
+        if (valgtKort == b) {
+            valgtKort = null; // Så fjern det
+            klik = false;
+            return; // Vi gør ikke mere
+        }
+
+        // Har vi ikke valgt et kort?
+        if (valgtKort == null) {
+            valgtKort = b;
+        } else {
+            // Er disse kort et par?
+            if (valgtKort.erParret(b)) {
+                valgtKort.fjern();  // Fjern dem fra spillet
+                b.fjern();
+            } else {
+                valgtKort.visBagside(); // Vend dem om igen
+                b.visBagside();
+            }
+            valgtKort = null;
+        }
+
+        klik = false;
     }
 }
